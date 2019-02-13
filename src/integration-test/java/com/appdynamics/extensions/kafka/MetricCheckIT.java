da1f87d1723ab423c409300ad675cb00f00f23e4
VishakaSekar <vishaka.sekar@appdynamics.com>
2019-02-13 09:17:49 -0800

    integration tests

--- src/integration-test/java/com/appdynamics/extensions/kafka/MetricCheckIT.java
+++ src/integration-test/java/com/appdynamics/extensions/kafka/MetricCheckIT.java
@@ -21,8 +21,6 @@ public class MetricCheckIT {
 
     File installDir = new File("src/integration-test/resources/conf/");
     File configFile = new File("src/integration-test/resources/conf/config_ci.yml");
-// File installDir = new File("src/main/resources/conf/");
-//    File configFile = new File("src/main/resources/conf/config.yml");
     metricAPIService = IntegrationTestUtils.setUpControllerClient(configFile,installDir);
 
     }
@@ -38,17 +36,12 @@ public class MetricCheckIT {
     JsonNode jsonNode = null;
     if(metricAPIService != null) {
         jsonNode = metricAPIService.getMetricData("",
-                    "Server%20&%20Infrastructure%20Monitoring/metric-data?metric-path=Application%20Infrastructure%20Performance%7CRoot%7CCustom%20Metrics%7CKafka%7CLocal%20Kafka%20Server2%7Ckafka.server%7CHeartBeat&time-range-type=BEFORE_NOW&duration-in-mins=1440&output=JSON");
+                    "Server%20&%20Infrastructure%20Monitoring/metric-data?metric-path=Application%20Infrastructure%20Performance%7CRoot%7CCustom%20Metrics%7CKafka%7CLocal%20Kafka%20Server2%7Ckafka.server%7CHeartBeat&time-range-type=BEFORE_NOW&duration-in-mins=15&output=JSON");
     }
     if (jsonNode != null) {
         JsonNode valueNode = JsonUtils.getNestedObject(jsonNode, "*", "metricValues", "*", "value");
         int heartBeat = (valueNode == null) ? 0 : valueNode.get(0).asInt();
         Assert.assertEquals("heartbeat is 0", heartBeat,1);
-    }
-
-    else if(jsonNode == null ){
-        System.out.println("JSON NODE NULL");
-
     }
 
     }
@@ -74,15 +67,34 @@ public class MetricCheckIT {
         JsonNode jsonNode = null;
         if(metricAPIService != null) {
             jsonNode = metricAPIService.getMetricData("",
-                    "Server%20&%20Infrastructure%20Monitoring/metric-data?metric-path=Application%20Infrastructure%20Performance%7CRoot%7CCustom%20Metrics%7CKafka%7CLocal%20Kafka%20Server2%7Ckafka.server%7CHeartBeat&time-range-type=BEFORE_NOW&duration-in-mins=15&output=JSON");
+                    "Server%20&%20Infrastructure%20Monitoring/metric-data?metric-path=Application%20Infrastructure%20Performance%7CRoot%7CCustom%20Metrics%7CKafka%7CLocal%20Kafka%20Server2%7Ckafka.server%7CKafkaRequestHandlerPool%7CRequestHandlerAvgIdlePercent%7COneMinuteRate&time-range-type=BEFORE_NOW&duration-in-mins=15&output=JSON");
         }
         if (jsonNode != null) {
             JsonNode valueNode = JsonUtils.getNestedObject(jsonNode, "*", "metricValues", "*", "value");
-            int heartBeat = (valueNode == null) ? 0 : valueNode.get(0).asInt();
-            Assert.assertEquals("heartbeat is 0", heartBeat,1);
+            int requestHandlerAvgPercent = (valueNode == null) ? 0 : valueNode.get(0).asInt();
+            Assert.assertTrue((requestHandlerAvgPercent > 90) && (requestHandlerAvgPercent <= 100));
+
         }
 
     }
+
+    @Test
+    public void checkTotalNumberOfMetricsReportedIsGreaterThan1(){
+        JsonNode jsonNode = null;
+        if(metricAPIService != null) {
+            jsonNode = metricAPIService.getMetricData("",
+                    "Server%20&%20Infrastructure%20Monitoring/metric-data?metric-path=Application%20Infrastructure%20Performance%7CRoot%7CCustom%20Metrics%7CKafka%7CMetrics%20Uploaded&time-range-type=BEFORE_NOW&duration-in-mins=15&output=JSON");
+        }
+        if (jsonNode != null) {
+            JsonNode valueNode = JsonUtils.getNestedObject(jsonNode, "*", "metricValues", "*", "value");
+            int totalNumberOfMetricsReported = (valueNode == null) ? 0 : valueNode.get(0).asInt();
+            Assert.assertTrue(totalNumberOfMetricsReported > 1);
+
+        }
+
+    }
+
+
 
 
 
